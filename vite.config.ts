cac83d48022ae42deb36faa504a03ecac9d0a01f
Andrew Nasef <androamazing@gmail.com>
2022-07-06 01:46:07 +0200

    vite path for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
+//@ts-check
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import { defineConfig } from 'vite'
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/sleepcalculator',
   plugins: [svelte(),
 
   ]
